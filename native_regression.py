3c78bcda48a43a7bd1bac20c621a9ab213242a45
erikmorck04 <erikmorck04@gmail.com>
2024-10-06 15:54:30 +0200

    Added matplotlib

--- native_regression.py
+++ native_regression.py
@@ -1,10 +1,18 @@
-import matrix
 from matrix import *
+import matplotlib.pyplot as plt
 
-matrix(loadtxt)
+matrix = transpose(loadtxt("textfiles/dataset1.txt"))
+X = matrix[0]
+Y = matrix[1]
 
 Xp  = powers(X,0,1)
 Yp  = powers(Y,1,1)
 Xpt = transpose(Xp)
 
 [[b],[m]] = matmul(invert(matmul(Xpt,Xp)),matmul(Xpt,Yp))
+
+print([[b],[m]])
+
+plt.plot(X,Y,'ro')
+plt.plot(X,Y2)
+plt.show()
