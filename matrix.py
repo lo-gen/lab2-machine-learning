9023b335a0c993dc43b5ce3c6af3bb9955dbfb8a
Grillkvall <wejlor0315@gmail.com>
2024-10-11 10:06:32 +0200

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -8,14 +8,14 @@ def transpose(M):
     m = 0
     new_matrix = []
     for n in range(0 , len(M)):
-        if len(M[n]) != len(M[0]):
+        if len(M[n]) != len(M[0]):          #Kollar så att alla rader är lika långa
             return "Not a matrice"
-    if M == []:
+    if M == []:                             #Ifall det är en tom matris går det inte att göra något med den
         return M
-    for n in range(0, len(M[n])):
+    for n in range(0, len(M[n])):           
         new_row = []
         for m in range(0, len(M)):
-            new_row.append(M[m][n])
+            new_row.append(M[m][n])         #Skiftar så att rad-kolumnvärde blir kolumn-radvärde i den nya matrisen
         new_matrix.append(new_row)
     return new_matrix
 
@@ -23,30 +23,30 @@ def powers(list, power1, power2):
     new_matrix = []
     for n in range(len(list)):
         new_list = []
-        for m in range(power1, power2 + 1):
-             new_list.append(list[n]**m)
+        for m in range(power1, power2 + 1): #Tar alla värden mellan power1 och power2
+             new_list.append(list[n]**m)    
         new_matrix.append(new_list)
     return new_matrix
 
 def matmul(M1, M2):
     new_matrix = []
-    if M1 == [] or M2 == []:
-        return []
-    elif len(M1[0]) == len(M2):
+    if M1 == [] or M2 == []:                #Ifall någon av "matriserna" är tomma så fungerar det inte
+        return []                           
+    elif len(M1[0]) == len(M2):             #Kolumnera i M1 måste vara lika långa som raderna i M2
         for n in range(len(M1)):
             new_row = []
             for m in range(len(M2[0])):
                 value = 0
-                for p in range(len(M2)):
-                    value = value + M1[n][p] * M2[p][m] 
+                for p in range(len(M2)):                    #Kan ha antingen len(M2) eller len(M1[0]), eftersom de är samma
+                    value = value + M1[n][p] * M2[p][m]     #Räknar ut värdet på positionen
                 new_row.append(value)
             new_matrix.append(new_row)
         return new_matrix
-    else:
+    else:                                   #Kommer den hit så matchar inte matriserna
         return "Wrong size on matrices"
 
 def invert(M):
-    if M == []:
+    if M == []:                             #Ifall "matrisen" är tom så fungerar det inte
         return M
     elif len(M) == 2 and len(M[0]) == 2:
         det = M[0][0] * M[1][1] - M[1][0] * M[0][1]
@@ -57,17 +57,17 @@ def invert(M):
         new_matrix.append(new_row2)
         return new_matrix
     else:
-        print("Det är inte en 2x2 matris")
+        return "It's not a 2x2 matrice"
 
 def loadtxt(file):
     new_matrix = []
-    file_open = open(file)
+    file_open = open(file)                  #Öppnar filen, läser den och sparar den i lines
     lines = file_open.readlines()
     file_open.close()
     for line in lines:
-        line = line.replace("\n", "")
-        list = line.split("\t")
+        line = line.replace("\n", "")       #Tar bort alla \n i texten
+        list = line.split("\t")             #Splittar den till listor 
         for n in range(0,2):
-            list[n] = float(list[n])
+            list[n] = float(list[n])        #Gör om allt från strings till decimaltal
         new_matrix.append(list)
     return new_matrix
